9732eff07fc1c62bf85287ce510fa39db0f2e136
unknown <danieleastwood4@gmail.com>
2022-05-06 21:22:52 -0400

    Redux commit

---
+++ src/Store.jsx
@@ -0,0 +1,11 @@
+import { configureStore } from '@reduxjs/toolkit'
+import counterReducer from './features/counterSlice'
+import initialState from './features/todoSlice'
+
+
+
+export const store = configureStore({
+    reducer: counterReducer,
+    todo: todoReducer
+})
+
---
+++ src/todo.jsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import {addTodo, removeOne, clearTodo} from './features/todoSlice'
+import React, {useState} from 'react'
+
+function Todo() {
+    const dispatch = useDispatch
+    const items = useSelector((state) => state.todos.items)
+    const [input, setInput] = useState('')
+
+    const renderItems = items.map((item,index) => <li key={index} onClick={() => dispatch(removeOne(index))}>{item}</li>)
+
+    const submitForm = (e) => {
+        e.preventDefault()
+        dispatch(addTodo(input))
+    }
+
+    return(
+        <div>
+            <form onSubmit={(e) => submitForm(e)}>
+                <input onChange={(e) => setInput(e.target.value) } type = 'text'></input>
+                <button type='submit'>Submit</button>
+            </form>
+            <ul>
+                {renderItems}
+            </ul>
+            <button onClick={() => dispatch(clearTodo())}>Clear</button>
+        </div>
+    )
+}
+
+export default Todo
